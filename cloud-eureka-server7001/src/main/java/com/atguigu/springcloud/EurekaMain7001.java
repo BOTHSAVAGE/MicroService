317f57d5122bc136cdfab88aaa25b7592250bcb6
BOTHSAVAGE <332450461@qq.com>
2021-07-28 01:28:23 +0800

    feat(cloud2020): init eureka-server
     - 增加eureka
    
    Closes p16

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author y
+ * @create 2021/7/28 1:18
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
