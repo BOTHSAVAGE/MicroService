212d9f19218c2f5a6bb43abe0b376da4847f6340
刘子仪 <332450461@qq.com>
2021-08-11 19:20:54 +0800

    fix(cloud2020): 客户端集成ribbon
     - ribbon的作用就是负载均衡选服务器的，一般和restTemplate协同使用
     - ribbon内置的有七种算法，除了简单的随机和轮询，就是依靠响应和服务器资源比率来做选择的
     - 重点就是IRule

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -1,8 +1,10 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author y
@@ -10,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@RibbonClient(name="CLOUD-PAYMENT-SERVICE",configuration = MyselfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
